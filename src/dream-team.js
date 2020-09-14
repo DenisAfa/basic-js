fa98d30659e2f7ad78d4c2f6622d39f1399257da
DenisAfa <denisafanasevm92@mail.ru>
2020-09-14 23:06:39 +0300

    feat:add function dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let arrLetter = [];
+  try {
+    for (let member of members) {
+      if(typeof(member) == "string") {
+        newMember = member.trim().toUpperCase();
+      arrLetter.push(newMember[0]);
+    }
+  }
+  } catch(err) {
+    return false;
+  }
+  let newName = arrLetter.sort().join("");
+  return newName;
+  
 };
