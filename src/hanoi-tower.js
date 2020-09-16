391fbef7d637491ca871ebd5260b1b8d63bb4f4e
DenisAfa <denisafanasevm92@mail.ru>
2020-09-16 08:31:45 +0300

    feat:add function hanoi-tower,what-season, start transform-array

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  let minTurn = (2 ** disksNumber - 1);
+  let minSpeed = Math.floor(minTurn / turnsSpeed * 3600);
+  let result = {
+    turns: minTurn,
+    seconds: minSpeed,
+  }
+  return result;
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let resultArr = arr;
+  if (Array.isArray(arr) == false) {
+    throw new Error("TypeError"); 
+  } else {
+    for (let i = 0; i < resultArr.length; i++) {
+      if (resultArr[i] == "--discard-next") {
+        resultArr.splice(i, 2);
+        arr.splice(i,1);
+      } else if (resultArr[i] == "--discard-prev") {
+        resultArr.splice(i-1, 2);
+        arr.splice(i,1);
+      } else if (resultArr[i] == "--double-next") {
+        resultArr.splice(i, 1, resultArr[i+1]);
+        arr.splice(i,1);
+      } else if (resultArr[i] == "--double-prev") {
+        resultArr.splice(i, 1, resultArr[i-1]);
+        arr.splice(i,1);
+      }
+    } 
+  }
+  
+  return resultArr
 };
